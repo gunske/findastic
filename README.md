ad4cfaba17c57e2eb32890012d1d2b108c3724ef
gunske <gunnar.skeid@gmail.com>
2017-10-24 17:43:21 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,89 @@
-# findastic
+# Findastic
+
 Simple Multi-phrase Search in any text
+
+Detect trigger words and phrases in text. Great for discovering of locations,
+social media mentions, simple language detection, filtering documents based on
+good/bad phrases etc.
+
+## Features
+
+* In-memory high performance search
+* Search for many multiple phrases at once
+* Lightweight 
+* Normalizes local language characters (E.g. á -> a)
+* Supports long texts with sentences
+* Attach data to phrases
+* Search phrases are indexed and can be exported/imported for higher performance
+
+## Install
+
+```
+$ npm install findastic
+```
+
+## Usage
+
+Initialize library:
+
+```
+const findastic = require('findastic');
+```
+
+Add phrases to search for:
+
+```
+findastic.add("javascript rocks");
+findastic.add("dream comes true");
+```
+
+Search text for those phrases:
+
+```
+findastic.search("I found that JavaScript Rocks", function (err, result) {
+    // Prints: Phrase 'javascript rocks' found at position 13
+    if (!err) console.log("Phrase '" + result[0].phrase + "' found at position " + result[0].position);
+});
+```
+
+### Advanced usage
+Attach data to phrases:
+
+```
+findastic.add("go north", {direction: "north"});
+findastic.add("go south", {direction: "south"});
+findastic.add("go west", {direction: "west"});
+findastic.add("go east", {direction: "east"});
+findastic.search("I want to go north to the reindeers", function (err, result) {
+    // Prints: "Direction is north"
+    if (!err) console.log("Direction is '" + result[0].data.direction);
+});
+```
+
+Delete and reset phrases:
+```
+findastic.reset();
+```
+
+Export and save index to file:
+```
+const fs = require('fs');
+const exportData = findastic.export();
+fs.writeFile("/tmp/findastic.txt", JSON.stringify(exportData), function(err) {
+    // ...
+});
+```
+
+Import index:
+```
+const fs = require('fs');
+fs.readFile("/tmp/findastic.txt", function(err, importData) {
+    if (!err) findastic.import(JSON.parse(importData));
+});
+```
+
+## License
+
+[The MIT License](http://opensource.org/licenses/MIT)
+
+Copyright (c) 2017 Gunnar Skeid
---
+++ test/test.js
@@ -0,0 +1,294 @@
+var assert = require('assert');
+
+describe('findastic-lib', function() {
+
+    const findastic = require('../lib');
+
+    describe('add/export', function () {
+
+        it('export empty index', function (done) {
+
+            const dump = findastic.export();
+
+            assert.notEqual(dump, null);
+            assert.equal(typeof dump.index, 'object');
+            assert.equal(typeof dump.phrases, 'object');
+
+            done();
+
+        });
+
+        it('search on empty index', function (done) {
+
+            findastic.search("dette liker vi heisann på deg", function (err, result) {
+
+                assert.equal(err, null);
+                assert.notEqual(result, null);
+                assert.ok(Array.isArray(result));
+                assert.equal(result.length, 0);
+
+                done();
+            });
+
+        });
+
+        it('add non-string phrase (throws)', function (done) {
+
+            assert.throws(function () {
+                findastic.add({hello: "world"}, null);
+            }, /Phrase must be a string/, 'did not throw with expected message');
+
+            done();
+
+        });
+
+        it('add undefined data (ok)', function (done) {
+
+            findastic.add("heisann", undefined);
+
+            const dump = findastic.export();
+
+            assert.notEqual(dump, null);
+            assert.equal(typeof dump.index, 'object');
+            assert.equal(typeof dump.phrases, 'object');
+            assert.equal(dump.phrases['1'], null);
+            assert.equal(dump.index['hei']['heisann'], 1);
+
+            done();
+
+        });
+
+        it('add object data (ok)', function (done) {
+
+            findastic.add("heida", {a:'b'});
+
+            const dump = findastic.export();
+
+            assert.notEqual(dump, null);
+            assert.equal(typeof dump.index, 'object');
+            assert.equal(typeof dump.phrases, 'object');
+            assert.equal(dump.phrases['2'].a, 'b');
+            assert.equal(dump.index['hei']['heida'], 2);
+
+            done();
+
+        });
+
+    });
+
+    describe('import', function () {
+
+        var firstDump;
+        it('init import', function (done) {
+
+            firstDump = findastic.export();
+            done();
+
+        });
+
+        it('import data wrong type', function (done) {
+
+            assert.throws(function () {
+                findastic.import("hei");
+            }, /Param data should be object/, 'did not throw with expected message');
+
+            done();
+
+        });
+
+        it('import data (lacking index -> fail)', function (done) {
+
+            assert.throws(function () {
+                findastic.import({phrases: {}});
+            }, /Param data.index should be object/, 'did not throw with expected message');
+
+            done();
+
+        });
+
+        it('import data (lacking phrases -> fail)', function (done) {
+
+            assert.throws(function () {
+                findastic.import({index: {}});
+            }, /Param data.phrases should be object/, 'did not throw with expected message');
+
+            done();
+
+        });
+
+        it('reset data', function (done) {
+
+            findastic.reset();
+            const dump = findastic.export();
+
+            assert.notEqual(dump, null);
+            assert.equal(typeof dump.index, 'object');
+            assert.equal(typeof dump.phrases, 'object');
+            assert.equal(Object.keys(dump.phrases).length, 0);
+            assert.equal(Object.keys(dump.index).length, 0);
+
+            done();
+
+        });
+
+        it('import data', function (done) {
+
+            findastic.import(firstDump);
+            const dump = findastic.export();
+
+            assert.notEqual(dump, null);
+            assert.equal(typeof dump.index, 'object');
+            assert.equal(typeof dump.phrases, 'object');
+            assert.equal(dump.phrases['2'].a, 'b');
+            assert.equal(dump.index['hei']['heida'], 2);
+
+            done();
+
+        });
+
+    });
+
+    describe('search', function () {
+
+        it('init search', function (done) {
+
+            findastic.reset();
+            findastic.add("a", {a:'a'});
+            findastic.add("ab", {a:'b'});
+            findastic.add("abs", {a:'s'});
+            findastic.add("abs ab", {a:'s2'});
+            findastic.add("heisann", {a:'h'});
+            findastic.add("heida", {a:'e'});
+            findastic.add("GründeRe", {a:'g'});
+            done();
+
+        });
+
+        it('search wrong input (fail)', function (done) {
+
+            findastic.search({hallo: "n"}, function(err, result) {
+
+                assert.notEqual(err, null);
+                done();
+
+            });
+
+        });
+
+        it('search no results', function (done) {
+
+            findastic.search("join absolute is king", function(err, result) {
+                assert.equal(err, null);
+                assert.notEqual(result, null);
+                assert.ok(Array.isArray(result));
+                assert.equal(result.length, 0);
+                done();
+            });
+
+        });
+
+        it('search abs', function (done) {
+
+            findastic.search("join abs is king", function(err, result) {
+
+                assert.equal(err, null);
+                assert.notEqual(result, null);
+                assert.equal(result[0].phrase, 'abs');
+                assert.equal(result[0].position, 5);
+                assert.equal(result[0].data.a, 's');
+                done();
+
+            });
+
+        });
+
+        it('search a', function (done) {
+
+            findastic.search("join  is ab king", function(err, result) {
+
+                assert.equal(err, null);
+                assert.notEqual(result, null);
+                assert.equal(result[0].phrase, 'ab');
+                assert.equal(result[0].position, 9);
+                assert.equal(result[0].data.a, 'b');
+                done();
+
+            });
+
+        });
+
+        it('search heisann er bra for heisann', function (done) {
+
+            findastic.search("join heisann er bra for heida", function(err, result) {
+
+                assert.equal(err, null);
+                assert.notEqual(result, null);
+                assert.equal(result[0].phrase, 'heisann');
+                assert.equal(result[0].position, 5);
+                assert.equal(result[0].data.a, 'h');
+                assert.equal(result[1].phrase, 'heida');
+                assert.equal(result[1].position, 24);
+                assert.equal(result[1].data.a, 'e');
+                done();
+
+            });
+
+        });
+
+        it('searchOne heisann er bra for heisann', function (done) {
+
+            findastic.searchOne("join heisann er bra for heida", function(err, result) {
+
+                assert.equal(err, null);
+                assert.notEqual(result, null);
+                assert.equal(result.length, 1);
+                assert.equal(result[0].phrase, 'heisann');
+                assert.equal(result[0].position, 5);
+                assert.equal(result[0].data.a, 'h');
+                done();
+
+            });
+
+        });
+
+        it('search GRUNDERE', function (done) {
+
+            findastic.search("GRUNDERE", function(err, result) {
+
+                assert.equal(err, null);
+                assert.notEqual(result, null);
+                assert.equal(result[0].phrase, 'grundere');
+                assert.equal(result[0].position, 0);
+                assert.equal(result[0].data.a, 'g');
+                done();
+
+            });
+
+        });
+
+        it('search two sentences', function (done) {
+
+            findastic.search("dette liker abs.ab la oss abs ab her heisann idag", function(err, result) {
+
+                assert.equal(err, null);
+                assert.notEqual(result, null);
+                assert.equal(result[0].phrase, 'abs');
+                assert.equal(result[0].position, 12);
+                assert.equal(result[0].data.a, 's');
+                assert.equal(result[1].phrase, 'ab');
+                assert.equal(result[1].position, 16);
+                assert.equal(result[1].data.a, 'b');
+                assert.equal(result[2].phrase, 'abs ab');
+                assert.equal(result[2].position, 26);
+                assert.equal(result[2].data.a, 's2');
+                assert.equal(result[3].phrase, 'heisann');
+                assert.equal(result[3].position, 37);
+                assert.equal(result[3].data.a, 'h');
+                done();
+
+            });
+
+        });
+
+    });
+});
